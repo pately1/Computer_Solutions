47265b704a537f10b2ad0d1360412a86e60aefa6
Yash <yashpatel256@gmail.com>
2016-12-04 19:10:47 -0500

    Adding MainController

---
+++ app/Home/MainController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Yash on 12/4/2016.
+ */
+(function () {
+    'use strict';
+
+    angular.module('Computer')
+        .controller('MainCtrl', function () {
+            console.log('Main Controller');
+        })
+})();
