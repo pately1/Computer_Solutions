9023b709a1d0f682e2a1e6482829e33829c90645
Yash <yashpatel256@gmail.com>
2016-12-04 20:06:31 -0500

    Adding seperate controller

---
+++ app/Contact/ContactController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Yash on 12/4/2016.
+ */
+(function () {
+    'use strict';
+
+    angular.module('Computer')
+        .controller('ContactCtrl', ['$scope', function ($scope) {
+
+        }]);
+})();
