fa46bb95e71a67a4898ececaca5d5ad15a14f878
Yash <yashpatel256@gmail.com>
2016-12-04 20:52:51 -0500

    getting data using service

--- app/Contact/ContactController.js
+++ app/Contact/ContactController.js
@@ -5,7 +5,15 @@
     'use strict';
 
     angular.module('Computer')
-        .controller('ContactCtrl', ['$scope', function ($scope) {
+        .controller('ContactCtrl', ['$scope', 'locationService', function ($scope, locationService) {
+            console.log("In Contact Controller");
 
+            locationService.getLocations().then(
+                function (response) {
+                    $scope.locations = response;
+                }, function (err) {
+                    console.log(err);
+                }
+            )
         }]);
 })();
